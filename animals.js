55373521609880c244279f40331d11500573fbfa
spas-durev <spasdurev@gmail.com>
2023-09-26 13:05:03 +0300

    Hello
    This is a test

---
+++ animals.js
@@ -0,0 +1,10 @@
+let dog {
+    name: 'Bucks',
+    age: 6,
+      }
+    
+      let cat {
+        name: 'Oliver',
+        age: 6,
+          }
+        
--- dogs.js
+++
@@ -1,5 +0,0 @@
-let dog {
-    name: 'Bucks',
-    age: 6,
-      }
-    
